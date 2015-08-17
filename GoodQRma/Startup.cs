d71d931630c2cc25c730c7fff4e37173a1411077
Vidget <vidget@yahoo.com>
2015-08-17 14:32:35 -0400

    GoodQRma
    With FACEBOOK authentication

---
+++ GoodQRma/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoodQRma.Startup))]
+namespace GoodQRma
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
