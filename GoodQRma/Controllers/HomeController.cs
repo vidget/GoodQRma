ee1c925f26e816fa89e426582fdd93ceec424271
anheshvi100 <hemakanagaraj@gmail.com>
2015-08-31 17:41:32 -0400

    try

--- GoodQRma/Controllers/HomeController.cs
+++ GoodQRma/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Net;
+using System.Net.Mail;
+using GoodQRma.Models;
+using System.Threading.Tasks;
 namespace GoodQRma.Controllers
 {
     public class HomeController : Controller
@@ -13,7 +16,7 @@ namespace GoodQRma.Controllers
 
 
             return View("Index", "_LayoutHOME");
-           
+
         }
 
         public ActionResult About()
@@ -26,8 +29,49 @@ namespace GoodQRma.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
+        [HttpGet]
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(HelpForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("goodqrma@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("hemaak91@yahoo.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "goodqrma@gmail.com",  // replace with valid value
+                        Password = "4goodqrma"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
     }
 }
---
+++ GoodQRma/Models/HelpForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoodQRma.Models
+{
+    public class HelpForm
+    {
+
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
+    
